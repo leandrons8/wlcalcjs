36f3e978aaa20967f2cc34af1498ecee008d158b
Leandro Nascimento Santos <93847137+leandrons8@users.noreply.github.com>
2024-12-10 14:03:38 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -14,8 +14,12 @@ function addAnilhas(){
     }
     row.insertCell(0).innerHTML = document.getElementById("cor").options[cor].innerHTML
     row.insertCell(1).innerHTML = ((padraoKgs) ? "Kgs" : "Lbs")
-    row.insertCell(2).innerHTML = kgs
-    row.insertCell(3).innerHTML = lbs
+    let cellKgs = row.insertCell(2)
+    cellKgs.dataset.w = kgs
+    cellKgs.innerHTML = Math.round(kgs)
+    let cellLbs = row.insertCell(3)
+    cellLbs.dataset.w = lbs
+    cellLbs.innerHTML = Math.round(lbs)
     row.insertCell(4).innerHTML = '<button onclick="deleteAnilha(this)" class="btn-close"></button>'
     updateSoma()
 }
@@ -33,8 +37,8 @@ function updateSoma(){
     let kgs = ((masculina) ? ((padraoKgs) ? 20 : 45/2.2046) : ((padraoKgs) ? 15 : 35/2.2046))
     let lbs = ((masculina) ? ((padraoKgs) ? 20*2.2046 : 45) : ((padraoKgs) ? 15*2.2046 : 35))
     for (let row of rows){
-        kgs += 2*parseFloat(row.getElementsByTagName("td")[2].innerHTML)
-        lbs += 2*parseFloat(row.getElementsByTagName("td")[3].innerHTML)
+        kgs += 2*parseFloat(row.getElementsByTagName("td")[2].dataset.w)
+        lbs += 2*parseFloat(row.getElementsByTagName("td")[3].dataset.w)
     }
     kgs = Math.round(kgs)
     lbs = Math.round(lbs)
