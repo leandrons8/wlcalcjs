d3c2f3682f69d0003a213bc43c49ce7c4afdcdd0
Leandro N. Santos <leandrons8@gmail.com>
2025-02-14 16:50:58 -0300

    Update

--- calc.js
+++ calc.js
@@ -1,8 +1,8 @@
 function addAnilhas(){
-    let cor = parseInt(document.getElementById("cor").value)
-    let padraoKgs = document.getElementById("anilhaKgs").checked
-    let table = document.getElementById("anilhas")
-    let row = table.insertRow()
+    const cor = parseInt(document.getElementById("cor").value)
+    const padraoKgs = document.getElementById("anilhaKgs").checked
+    const table = document.getElementById("anilhas")
+    const row = table.insertRow()
     let kgs
     let lbs
     if (padraoKgs) {
@@ -12,28 +12,30 @@ function addAnilhas(){
         lbs = 10*(cor+1) + 5
         kgs = lbs/2.2046
     }
-    row.insertCell(0).innerHTML = document.getElementById("cor").options[cor].innerHTML
-    row.insertCell(1).innerHTML = ((padraoKgs) ? "Kgs" : "Lbs")
-    let cellKgs = row.insertCell(2)
+    row.insertCell(0).innerText = document.getElementById("cor").options[cor].innerText
+    row.insertCell(1).innerText = ((padraoKgs) ? "Kgs" : "Lbs")
+    const cellKgs = row.insertCell(2)
     cellKgs.dataset.w = kgs
-    cellKgs.innerHTML = Math.round(kgs)
-    let cellLbs = row.insertCell(3)
+    cellKgs.innerText = Math.round(kgs)
+    const cellLbs = row.insertCell(3)
     cellLbs.dataset.w = lbs
-    cellLbs.innerHTML = Math.round(lbs)
-    row.insertCell(4).innerHTML = '<button onclick="deleteAnilha(this)" class="btn-close"></button>'
-    updateSoma()
-}
-
-function deleteAnilha(button){
-    let row = button.parentNode.parentNode
-    row.parentNode.removeChild(row)
+    cellLbs.innerText = Math.round(lbs)
+    const remove = row.insertCell(4)
+    const button = document.createElement("button")
+    button.className = "btn-close"
+    button.onclick = function (){
+        let row = this.parentNode.parentNode
+        row.parentNode.removeChild(row)
+        updateSoma()
+    }
+    remove.append(button)
     updateSoma()
 }
 
 function updateSoma(){
-    let masculina = document.getElementById("masculina").checked
-    let padraoKgs = document.getElementById("barraKgs").checked
-    let rows = document.getElementById("anilhas").rows
+    const masculina = document.getElementById("masculina").checked
+    const padraoKgs = document.getElementById("barraKgs").checked
+    const rows = document.getElementById("anilhas").rows
     let kgs = ((masculina) ? ((padraoKgs) ? 20 : 45/2.2046) : ((padraoKgs) ? 15 : 35/2.2046))
     let lbs = ((masculina) ? ((padraoKgs) ? 20*2.2046 : 45) : ((padraoKgs) ? 15*2.2046 : 35))
     for (let row of rows){
@@ -42,7 +44,7 @@ function updateSoma(){
     }
     kgs = Math.round(kgs)
     lbs = Math.round(lbs)
-    document.getElementById("total").innerHTML = "Peso total da barra: " + kgs + "kgs | " + lbs + "lbs"
+    document.getElementById("total").innerText = "Peso total da barra: " + kgs + "kgs | " + lbs + "lbs"
 }
 
 window.onload = updateSoma()
